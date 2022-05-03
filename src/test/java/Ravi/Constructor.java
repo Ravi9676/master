faba1957f1919c39ca048c26d47ee79c1719d1e5
Ravi <pothineni076@gmail.com>
2022-05-03 15:43:10 +0530

    "Adding parameterized Constructor"

--- src/test/java/Ravi/Constructor.java
+++ src/test/java/Ravi/Constructor.java
@@ -5,7 +5,10 @@ int a=10;
     public Constructor(){
         int a=20;
     }
-    
+    public  Constructor(int a,int b){
+        a=10;
+        b=20;
+    }
     public Constructor(int a){
      a=20;
 }
