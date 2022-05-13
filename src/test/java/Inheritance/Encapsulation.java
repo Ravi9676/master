08d6edeff349f2bbdca979e737e7be128f5da9c6
navya <navyasrikongara123@gmail.com>
2022-05-13 12:51:07 +0530

    Encapsulation

---
+++ src/test/java/Inheritance/Encapsulation.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class Encapsulation {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
